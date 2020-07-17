f7313aaf08c1bd5a11929e5a04ad08d00501f1cc
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2020-07-17 18:06:48 +0800

    fix

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export enum ButtonSize {
   Large = 'lg',
@@ -6,5 +7,48 @@ export enum ButtonSize {
 }
 
 export enum ButtonType {
-  
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
 }
+
+interface BaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+
+const Button: React.FC<BaseButtonProps> = (props) => {
+  const { btnType, disabled, size, children, href } = props;
+
+  const classes = classNames('btn', {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    disabled: btnType === ButtonType.Link && disabled
+  });
+
+  if (btnType === ButtonType.Link && href) {
+    return (
+      <a className={classes} href={href}>
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button className={classes} disabled={disabled}>
+        {children}
+      </button>
+    );
+  }
+};
+
+Button.defaultProps = {
+  btnType: ButtonType.Default,
+  disabled: false
+};
+
+export default Button;
