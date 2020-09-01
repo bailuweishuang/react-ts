ad9984839f5ad885bcb2a47c545c18acb54e383a
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2020-09-01 15:02:12 +0800

    fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.tsx', '../src/components/**/*.stories.tsx'],
+  stories: ['../src/components/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async (config) => {
     // do mutation to the config
