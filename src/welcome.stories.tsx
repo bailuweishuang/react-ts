e065adc426164be9326c9f425ed8b5109ac7b84c
huyongxiang@zdsoft.cn <huyongxiang@zdsoft.cn>
2020-09-28 17:17:52 +0800

    fix

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -7,7 +7,6 @@ storiesOf('Welcome page', module).add(
     return (
       <>
         <h1>欢迎来到 huyx-react-ts 组件库</h1>
-        <p>huyx-react-ts 是为慕课网课程打造的一套教学组件库，从零到一让大家去学习</p>
         <h3>安装试试</h3>
         <code>npm install huyx-react-ts --save</code>
       </>
